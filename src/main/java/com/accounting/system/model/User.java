467d5c6c7d5ff0b3a053ea778dcdc38436640737
cjh031116 <jp2022213089@qmul.ac.uk>
2025-03-24 15:39:28 +0800

    添加登录界面和控制器实现

---
+++ src/main/java/com/accounting/system/model/User.java
@@ -0,0 +1,36 @@
+package com.accounting.system.model;
+
+public class User {
+    private String username;
+    private String passwordHash;
+    private boolean rememberMe;
+
+    public User(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+} 
