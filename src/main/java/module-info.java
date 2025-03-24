4f0c1a870034f022305a96445bcba57da274fd5b
teliduxingdediao <jp2022213048@qmul.ac.uk>
2025-03-24 15:12:04 +0800

    login in(登录)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.accounting.system.accountingsystem {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.accounting.system to javafx.fxml;
+    opens com.accounting.system.controller to javafx.fxml;
     exports com.accounting.system;
+    exports com.accounting.system.controller;
+    exports com.accounting.system.model;
 }
