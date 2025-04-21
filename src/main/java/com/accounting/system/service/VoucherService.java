86fd66e11b202b6c5085581376a9fd0e07581439
whw422 <jp2022213139@qmul.ac.uk>
2025-04-21 18:15:06 +0800

    update project file error

---
+++ src/main/java/com/accounting/system/service/VoucherService.java
@@ -0,0 +1,21 @@
+package com.accounting.system.service;
+
+import com.accounting.system.model.Voucher;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface VoucherService {
+    Voucher createVoucher(Voucher voucher);
+    void updateVoucher(Voucher voucher);
+    void deleteVoucher(Long id);
+    Optional<Voucher> getVoucherById(Long id);
+    List<Voucher> getAllVouchers();
+    List<Voucher> searchVouchers(String keyword, LocalDate startDate, LocalDate endDate);
+    void verifyVoucher(Long id, Long verifiedBy);
+    String generateVoucherNumber();
+    boolean isBalanced(Voucher voucher);
+    Voucher getVoucherById(Long id);
+    Voucher saveVoucher(Voucher voucher);
+    List<Voucher> searchVouchers(String keyword);
+} 
