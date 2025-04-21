058fcc3e574cb649f1cc1228ce8e52144ac2bfe3
Qw0nd <Qw0nd>
2025-04-21 20:17:24 +0800

    add account

---
+++ src/main/java/com/accounting/system/model/Account.java
@@ -0,0 +1,16 @@
+package com.accounting.system.model;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String accountName;
+    private String password;
+    private String phone;
+    private String email;
+    private String address;
+    private String remark;
+
+
+}
